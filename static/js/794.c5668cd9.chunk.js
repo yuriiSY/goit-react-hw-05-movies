ac0b492ed725e38a7f31c2671661602f22db41cb
yuriiSY <108358132+yuriiSY@users.noreply.github.com>
2024-02-02 21:43:42 +0000

    Deploying to gh-pages from @ yuriiSY/goit-react-hw-05-movies@deda9704bf524213b27c26e7616ca463dfb621d4 🚀

---
+++ static/js/794.c5668cd9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{794:function(e,t,n){n.r(t);var r=n(87),a=n(184);t.default=function(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{children:"Cannot found this page"}),(0,a.jsx)(r.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=794.c5668cd9.chunk.js.map
